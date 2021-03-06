562ea9477161012c1c4a9724771d90d6ea00777e
Simon <simon_geier@web.de>
2021-03-06 09:56:13 +0100

    Added untracked file

---
+++ include/Violent/Utils/VUtils.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <string>
+#include <sstream>
+#include <type_traits>
+
+#include <locale>
+#include <codecvt>
+
+
+namespace At0::Violent
+{
+	std::string ConvertUtf8(const std::wstring& wstr)
+	{
+		static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+		return converter.to_bytes(wstr);
+	}
+
+#include "VSerialize.inl"
+
+	template<typename... Args>
+	inline std::string SerializeString(std::string serializedStr, Args&&... args)
+	{
+		int argCount = 0;
+		(SerializeStringArg(serializedStr, args, argCount), ...);
+		return serializedStr;
+	}
+}  // namespace At0::Violent
